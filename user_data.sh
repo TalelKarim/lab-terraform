64a2981db16fe3814f86acd28b1a91aa99966271
tchebbi <talelkarim.chebbi@insat.ucar.tn>
2024-01-18 11:38:56 +0100

    Revert "Use custom image"
    This reverts commit bab0d7ceb6a8fba0c2d162f6fc011e04f685cbf3.

--- user_data.sh
+++ user_data.sh
@@ -4,6 +4,7 @@ sudo apt-get install -y git
 sudo apt-get install -y docker.io
 sudo systemctl enable docker
 sudo systemctl start docker
+sudo touch tuto.txt
 sudo chmod 777 /var/run/docker.sock
 sudo docker pull talelch/pixalion_client:v1
 sudo docker run --name pixalion_front -p 3000:3000 talelch/pixalion_client:v1
